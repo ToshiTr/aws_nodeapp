a6cf11273aac096e682250486ffeb06d2f800f24
ToshiTr <143547503+ToshiTr@users.noreply.github.com>
2023-10-26 16:47:22 +0530

    index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,7 +12,7 @@ $("#update_student_result").submit(function (event) {
         data[n['name']] = n['value'];
     })
     var request = {
-        "url": `${apiBaseUrl}/api/update-result/${data.id}`,
+        "url": `http://13.127.164.162:3000/api/update-result/${data.id}`,
         "method": "PUT",
         "data": data,
     }
@@ -31,7 +31,7 @@ if (window.location.pathname == "/teacher") {
         var id = $(this).attr("data-id");
 
         var request = {
-            "url": `${apiBaseUrl}/api/delete-result/${id}`,
+            "url": `http://13.127.164.162:3000/api/delete-result/${id}`,
             "method": "DELETE"
         }
 
